eb22f35c8e3f0766408271d79e7d981c0da4e646
Adam Bremer <Adam.Bremer@ibm.com>
2020-07-16 16:49:55 +0200

    numInput
    not working

--- src/content/aPage1/aPage1.js
+++ src/content/aPage1/aPage1.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NumInput from '../../components/NumInput/Numinput'
 
 const Page1 = () => {
     return(
         <div>
+            <NumInput></NumInput>
             <h1>Page1</h1>
         </div>
     );
--- src/content/aPage1/index.js
+++ src/content/aPage1/index.js
@@ -1,3 +1,5 @@
 import Page1 from './aPage1';
 import NumInput from '../../components/NumInput/Numinput'
+
+
 export default Page1
