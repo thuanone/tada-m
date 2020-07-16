c097b5164c5f110c3682493941cff0815e5d2c58
Adam Bremer <Adam.Bremer@ibm.com>
2020-07-16 12:23:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import Users from './users'
 
-const routing = (
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/users" component={Users} />
-    </div>
-  </Router>
-)
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
