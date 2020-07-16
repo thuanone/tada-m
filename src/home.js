ff990289c5a8a87f8316ce73f097a434ab85ccc8
Adam Bremer <Adam.Bremer@ibm.com>
2020-07-16 14:06:44 +0200

    update
    added icons, changed font, align center

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './home.css';
+import './home.scss';
 
 class Home extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+
+import { Document32 } from "@carbon/icons-react";
 
 import Home from './home';
 import aPage1 from './content/aPage1';
@@ -9,25 +11,29 @@ import aPage2 from './content/aPage2';
 import aPage3 from './content/aPage3';
 
 
-
 const routing = (
   <Router>
     <div>
       <ul>
 
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/aPage1">aPage1</Link>
-        </li>
-        <li>
-          <Link to="/aPage2">aPage2</Link>
-        </li>
-        <li>
-          <Link to="/aPage3">aPage3</Link>
-        </li>
+        <div class="bx--grid">
+          <div class="bx--row">
 
+            <div class="bx--col">
+                <Link to="/"> <Document32 /> Home</Link>
+            </div>
+            <div class="bx--col">
+              <Link to="/aPage1"> <Document32 /> aPage1</Link>
+            </div>
+            <div class="bx--col">
+              <Link to="/aPage2"> <Document32 /> aPage2</Link>
+            </div>
+            <div class="bx--col">
+              <Link to="/aPage3"> <Document32 /> aPage3</Link>
+            </div>
+
+          </div>
+        </div>
       </ul>
 
       <Route exact path="/" component={Home} />
