f3e89a9829c902d8acd14cb3d4b3a633a11e3a3d
Adam Bremer <Adam.Bremer@ibm.com>
2020-07-16 17:54:57 +0200

    updated homepage
    +submit

--- src/home.js
+++ src/home.js
@@ -3,14 +3,26 @@ import './home.scss';
 
 
 class Home extends React.Component {
+  state = {
+    check1: true
+  };
+
+  handleCheck = (event) => {
+    this.setState({check1: event.target.checked});
+  };
+  handleSubmit = (event) => {
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div>
         <h1>Home</h1>
         <div className="tasks">
           <h2>Tasks</h2>
-          <input type="checkbox" checked="true"/> <a>creating a react app</a>
+          <input type="checkbox" onClick={this.handleCheck} checked={this.state.check1}/> <a>creating a react app</a>
         </div>
+        <button className="button1" onClick= {this.handleSubmit}>Submit</button>
       </div>
     )
   }
