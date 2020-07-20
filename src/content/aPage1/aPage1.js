5666031ed912bfbae2d75d676a97d0066cbd874d
Adam Bremer <Adam.Bremer@ibm.com>
2020-07-20 16:25:56 +0200

    big update vA
    + Range (min max)
    + reset button
    + none radiobutton
    + 'unknown unit' detection

--- src/content/aPage1/aPage1.js
+++ src/content/aPage1/aPage1.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './aPage1.scss'
-import {Tile} from 'carbon-components-react'
+import {Tile, RadioButton} from 'carbon-components-react'
 
 const unitList = ['GiB','MiB','vCPU','s'];
+
+const minNum = -1
+const maxNum = 100
 
 class Page1 extends React.Component {
 
@@ -13,6 +16,7 @@ class Page1 extends React.Component {
         MiB: false,
         vCPU: false,
         sec: false,
+        radioNone: false,
     };
 
     
@@ -28,30 +32,47 @@ class Page1 extends React.Component {
         } 
         
         else {
-            this.setState({unit: 'unknown unit'})
+            this.setState({unit: 'unknown unit', radioNone:true})
         };
 
     };
 
     increaseNum = (event) =>{
         var inputField = this.state.name.split(' ');
-        this.setState({name: String( parseFloat(inputField[0]) +10) + ' ' + inputField[1] 
-        })
+
+        if (inputField[0] ===''){
+            this.setState({unit: 'please specify unit'})
+        }
+
+        if ( (parseFloat(inputField[0]) + 10) <= maxNum ){
+            this.setState({name: String( parseFloat(inputField[0]) +10) + ' ' + inputField[1] })
+        }
     }
     decreaseNum = (event) =>{
         var inputField = this.state.name.split(' ');
-        this.setState({name: String( parseFloat(inputField[0]) -10) + ' ' + inputField[1] 
-        })
+
+        if (inputField[0] ===''){
+            this.setState({unit: 'please specify unit'})
+        }
+
+        if ( (parseFloat(inputField[0]) -10) >= minNum ){
+            this.setState(  {name: String( parseFloat(inputField[0]) -10) + ' ' + inputField[1] })
+        }
     }
 
 
     handleCheckUnit = (event) => {
         let value = event.target.value
         if (unitList.includes(value)){
-            this.setState({name:'0 '+ value})
+            this.setState({name:'0 '+ value, radioNone: false})
+        }else{
+            this.setState({unit: 'please specify unit', radioNone: true, name:''})
         }
-
     }
+
+    resetVal = (event) =>{
+        this.setState({name:'', radioNone: true})
+    } 
 
 
     render(){
@@ -60,14 +81,16 @@ class Page1 extends React.Component {
                 <Tile>Input field</Tile> <br/>
 
                 <fieldset>
-                    <input type="radio" name = "Units" onClick={this.handleCheckUnit} value ='GiB'/>
+                    <input className='radiobtn' type="radio" name = "Units" onClick={this.handleCheckUnit} value ='GiB'/>
                     <a>GiB</a>
-                    <input type="radio" name = "Units" onClick={this.handleCheckUnit} value = 'MiB'/>
+                    <input className='radiobtn' type="radio" name = "Units" onClick={this.handleCheckUnit} value = 'MiB'/>
                     <a>MiB</a>
-                    <input type="radio" name = "Units" onClick={this.handleCheckUnit} value = 'vCPU' />
+                    <input className='radiobtn' type="radio" name = "Units" onClick={this.handleCheckUnit} value = 'vCPU' />
                     <a>vCPU</a>
-                    <input type="radio" name = "Units" onClick={this.handleCheckUnit} value = 's' />
+                    <input className='radiobtn' type="radio" name = "Units" onClick={this.handleCheckUnit} value = 's' />
                     <a>Seconds</a>
+                    <input className='radiobtn0' type="radio" name = "Units" onClick={this.handleCheckUnit} value = 'none' checked ={this.state.radioNone}/>
+                    <a>none</a>
                 </fieldset>
 
                 <button onClick={this.decreaseNum}> - </button>
@@ -76,9 +99,10 @@ class Page1 extends React.Component {
                     value= {this.state.name} 
                     onChange = {this.handleChange}
                 />
-                <button onClick= {this.increaseNum}> + </button>
+                <button onClick= {this.increaseNum}> + </button> <br/>
 
-                <button className="button1" onClick= {this.handleRequest}>send request</button>
+                <button className="send-btn" onClick = {this.handleRequest}>send request</button>
+                <button className='reset-btn' onClick ={this.resetVal}> reset </button>
 
                 <Tile>{this.state.unit}</Tile>
             </div>
