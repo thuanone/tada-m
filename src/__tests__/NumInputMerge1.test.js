43249e834ec7fb0536d4b38e990765e4b59e7b88
thuanone <67963006+thuanone@users.noreply.github.com>
2020-08-09 14:47:55 +0200

    added enzyme-react-adapter-16 and babel.config.js file

---
+++ src/__tests__/NumInputMerge1.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NumInputMerge1 from '../components/NumInputMerge1';
+
+Enzyme.configure({adapter: new Adapter()});
