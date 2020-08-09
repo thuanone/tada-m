e3b8e3bc8f423648771a5ffc99905c0993a5fad8
thuanone <67963006+thuanone@users.noreply.github.com>
2020-08-09 16:11:21 +0200

    few successful tests on NumInput6

--- src/__tests__/NumInputMerge1.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import NumInputMerge1 from '../components/NumInputMerge1';
-
-Enzyme.configure({adapter: new Adapter()});
