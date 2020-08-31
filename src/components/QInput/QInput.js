843fc1f63599dfce0176e9eb3abb645013d130fc
Adam Bremer <Adam.Bremer@ibm.com>
2020-08-31 13:46:20 +0200

    implemented invalid symbol (!)

--- src/components/QInput/QInput.js
+++ src/components/QInput/QInput.js
@@ -96,7 +96,6 @@ class QInput extends React.Component {
     }
     return checked;
   }
-  
 
   /**
    * this functions receives a number and oth parameters and returns an object containing
@@ -387,7 +386,13 @@ class QInput extends React.Component {
       report.message = `recognized unit: ${word}`;
       report.unitPTR = indexOfMatchedUnit;
     }
-    checked = this.checkMinMax(number, minVal, maxVal, this.unitMatch(word, unitConfig), unitConfig);
+    checked = this.checkMinMax(
+      number,
+      minVal,
+      maxVal,
+      this.unitMatch(word, unitConfig),
+      unitConfig
+    );
     if (checked.message === "") {
       return report;
     } else {
@@ -592,6 +597,25 @@ class QInput extends React.Component {
                     this.delayedCall();
                   }}
                 />
+                <svg
+                  opacity = {this.state.isValid ? 0 : 1}
+                  focusable="false"
+                  preserveAspectRatio="xMidYMid meet"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  aria-hidden="true"
+                  class="bx--number__invalid"
+                >
+                  <path d="M8,1C4.2,1,1,4.2,1,8s3.2,7,7,7s7-3.1,7-7S11.9,1,8,1z M7.5,4h1v5h-1C7.5,9,7.5,4,7.5,4z M8,12.2    c-0.4,0-0.8-0.4-0.8-0.8s0.3-0.8,0.8-0.8c0.4,0,0.8,0.4,0.8,0.8S8.4,12.2,8,12.2z"></path>
+                  <path
+                    d="M7.5,4h1v5h-1C7.5,9,7.5,4,7.5,4z M8,12.2c-0.4,0-0.8-0.4-0.8-0.8s0.3-0.8,0.8-0.8    c0.4,0,0.8,0.4,0.8,0.8S8.4,12.2,8,12.2z"
+                    data-icon-path="inner-path"
+                    opacity="0"
+                  ></path>
+                </svg>
                 <div class="bx--number__controls">
                   <button
                     class="bx--number__control-btn up-icon"
